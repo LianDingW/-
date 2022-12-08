3124984a43c77e940976641219aea595903af38e
LianDingW <113701951+LianDingW@users.noreply.github.com>
2022-12-09 00:46:46 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,9 +20,9 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=24730153&pid=26d920267a874f3d7d5474a84c24451e&tid=161094f37baf1130303db5e49b27d6f3",
-    "https://www.maimemo.com/share/page?uid=24730153&pid=26d920267a874f3d7d5474a84c24451e&tid=161094f37baf1130303db5e49b27d6f3",
-    "https://www.maimemo.com/share/page?uid=24730153&pid=26d920267a874f3d7d5474a84c24451e&tid=161094f37baf1130303db5e49b27d6f3",
+    "https://www.maimemo.com/share/page?uid=24730153&pid=9d5e60f94bd3b2a9a6fc9ac50e369075&tid=1e15c64a12c3ac5956902022b6dd6f59",
+    "https://www.maimemo.com/share/page?uid=24730153&pid=9d5e60f94bd3b2a9a6fc9ac50e369075&tid=1e15c64a12c3ac5956902022b6dd6f59",
+    "https://www.maimemo.com/share/page?uid=24730153&pid=9d5e60f94bd3b2a9a6fc9ac50e369075&tid=1e15c64a12c3ac5956902022b6dd6f59",
     
  ]
   /**
